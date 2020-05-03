6fab350123d58e4e1fe8a5bee05d292c4dfff367
4gac <feri4gac@gmail.com>
2020-05-03 12:34:13 +0200

    header change

--- gcxml/src/gcxml/XMLCreator.java
+++ gcxml/src/gcxml/XMLCreator.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * XMLCreator.java
+ * 2020 Frantisek Forgac, Normex
  */
 package gcxml;
 
