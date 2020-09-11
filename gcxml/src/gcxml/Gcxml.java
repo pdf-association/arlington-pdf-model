819fce97e307433de9b107664f5ee5793010f6d4
4gac <feri4gac@gmail.com>
2020-09-11 14:13:48 +0200

    improved conversion from tsv to xml
    - fixed an error where some values were lost during the process of
    conversion
    - functions are not being evaluated (yet). They are just converted in
    their "raw" form.

--- gcxml/src/gcxml/Gcxml.java
+++ gcxml/src/gcxml/Gcxml.java
@@ -24,7 +24,7 @@ public class Gcxml {
 	/**
 	 * gcxml version string
 	 */
-    public static final String grammar_version = "0.3.0";
+    public static final String grammar_version = "0.4.0";
 
     /**
      * @param args the command line arguments
