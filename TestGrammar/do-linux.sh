af0e3515d7cc77e2424a5ca2947cd09ded566280
Peter Wyatt <26521615+petervwyatt@users.noreply.github.com>
2022-08-23 19:57:34 +1000

    Delete do-linux.sh

--- TestGrammar/do-linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cmake -B cmake-linux/debug -DPDFSDK_PDFIX=ON -DCMAKE_BUILD_TYPE=Debug .
-cmake --build cmake-linux/debug --config Debug
-cmake -B cmake-linux/release -DPDFSDK_PDFIX=ON -DCMAKE_BUILD_TYPE=Release .
-cmake --build cmake-linux/release --config Release
-ls -lsa bin/linux
-rm -rf cmake-linux
-
